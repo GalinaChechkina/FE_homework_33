2d62a2c4c75f81f45b5f71eeb8ce735e210048cc
GalinaChechkina <galina.chechkina@gmail.com>
2024-03-13 12:01:30 +0100

    add_comments

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -4,11 +4,22 @@ import { loadProducts, selectProducts } from '../store/slices/productsSlice'
 import { RootDispatch} from '../store/store';
 
 export default function ProductsList() {
-
+    //хук, кот. позволяет функциональным компонентам получать доступ к состоянию
+    //и извлекать данные из хранилища
+    //аргумент- функция селектора, кот. принимает глобальное состояние и возвращает необходимые данные
+    //при изменении состояния компонент будет перерисовываться только в случае, когда
+    //возвращаемое значение селектора изменяется 
     const productsList = useSelector(selectProducts);
 
     const dispatch = useDispatch<RootDispatch>();//- проводник, кот. ничего не меняет
-
+    
+    //хук, кот. позволяет выполнять побочные эффекты (у нас- запрос к серверу) 
+    //в функциональных компонентах, принимает функцию-эффект и массив зависимостей 
+    //(ф-я будет выполняться каждый раз, когда произойдут изменения, указанные в массиве зависимостей)
+    //если массив отсутствует, ф-я будет выполняться каждый раз при перерисовке компонента
+    //если массив пуст, ф-я выполнится один раз при монтировании компонента
+    //если массив не пуст, ф-я будет выполняться каждый раз, когда произойдут изменения,
+    //указанные в массиве зависимостей
     useEffect(()=> {
         dispatch(loadProducts());
     }, [dispatch]
@@ -16,10 +27,10 @@ export default function ProductsList() {
 
   return (
     <div>
-        <h3>ProductsList</h3>
+        <h2 style={{margin:"50px"}}>ProductsList</h2>
         
         {productsList.map((e)=> (
-           <div key={e.id}>
+           <div style={{border:"1px solid black", width: "500px", margin: "50px"}} key={e.id}>
                 <p>Title: {e.title}</p>
                 <p>Price: {e.price}</p>
                 <p>Rating: rate: {e.rating.rate}, count: {e.rating.count}</p>
